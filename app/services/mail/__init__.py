d7e2592921716775062ea9ca343be6924dd2f8d3
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-07-20 07:08:57 +0300

    refactor(mail): common mail package

---
+++ app/services/mail/__init__.py
@@ -0,0 +1,5 @@
+from .email_service import EmailService
+from .sendgrid_email_service import SendGridEmailService
+from .mailchimp_email_service import MailChimpEmailService
+from .smtp_proxy import SmtpServer
+from .mailer import send_plain_mail
